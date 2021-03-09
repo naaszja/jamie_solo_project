e61b4dcf442bb8cde9f6551664fc39e8c0172120
Jamie <naaszja@gmail.com>
2021-03-09 09:23:28 -0600

    changed favicon

---
+++ src/components/UserPage/QR.test.js
@@ -0,0 +1,5 @@
+// import {showQr} from './UserPage';
+
+// test('', () => {
+//     expect(showQr({type: 'FETCH_SINGLE_WORKORDER', payload: 7}).toBe(`https://nameless-tor-68673.herokuapp.com/#/workOrder/7`))
+// })
